ee3ecdc4d0bbe5c1708b314509b20a28c0498b20
ckpatel21 <chintanpatel2199@gmail.com>
2023-02-07 17:50:25 -0500

    Contains class

---
+++ A2_Final1744/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_Final1744
+{
+    public class Circle
+    {
+        private int radius { get; set; }
+
+        public Circle(int radius)
+        {
+            this.radius = 1;
+        }
+
+        public Circle()
+        {
+            this.radius = 1;
+        }
+
+        public int GetRadius()
+        {
+            return this.radius;
+        }
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * 3.14 * this.radius;
+        }
+
+        public double GetArea()
+        {
+            return 3.14 * this.radius * this.radius;
+        }
+    }
+}
--- A2_Final1744/Program.cs
+++ A2_Final1744/Program.cs
@@ -10,6 +10,46 @@ namespace A2_Final1744
     {
         static void Main(string[] args)
         {
+            Circle circle = new Circle();
+            Boolean flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n=========================\n");
+                Console.WriteLine("1. Get Circle Radius");
+                Console.WriteLine("2. Change Circle Radius");
+                Console.WriteLine("3. Get Circle Circumference");
+                Console.WriteLine("4. Get Circle Area");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\n=========================\n");
+
+                int num = Convert.ToInt16(Console.ReadLine());
+
+
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine(circle.GetRadius().ToString());
+                        break;
+                    case 2:
+                        Console.Write("Please Enter New Circle Radius :");
+                        int radius = Convert.ToInt16(Console.ReadLine());
+                        circle.SetRadius(radius);
+                        break;
+                    case 3:
+                        Console.WriteLine(circle.GetCircumference().ToString());
+                        break;
+                    case 4:
+                        Console.WriteLine(circle.GetArea().ToString());
+                        break;
+                    case 5:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter other value");
+                        break;
+                }
+
+            }
         }
     }
 }
